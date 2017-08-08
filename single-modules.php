4d357627e56c6e2d74f797fc2678033e33ec2750
jav <javidyousaf@outlook.com>
2017-08-08 15:01:09 +0100

    module dividers

--- single-modules.php
+++ single-modules.php
@@ -5,9 +5,12 @@ get_header(); ?>
     <div id="primary" class="col-md-9 content-area">
         <?php while ( have_posts() ) : the_post(); ?>
             <div class="post-comments">
-                <h2 align="center"><?php the_title(); ?></h2> 
+                <h2 align="center"><?php the_title(); ?></h2>
+                <hr />
                 <h4>Description</h4><?php the_content(); ?>
+                <hr />
                 <h4>Address</h4><?php echo get_field('module_address'); ?>
+                <hr />
                 <h4>Contact Name</h4><?php echo get_field('module_contact_name'); ?>
             </div>
         <?php endwhile; ?>

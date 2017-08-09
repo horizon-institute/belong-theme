254e604ff57046b3cdd4548abfabf5201610c2b1
jav <javidyousaf@outlook.com>
2017-08-09 08:35:19 +0100

    import parent style

--- functions.php
+++ functions.php
@@ -1,18 +1,7 @@
 <?php
-    // function lighthouse_style_enqueue_styles() {
-    //     $parent_style = 'lighthouse-style'; 
-    //     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    //     wp_enqueue_style( 'child-style',
-    //         get_stylesheet_directory_uri() . '/style.css',
-    //         array( $parent_style ),
-    //         wp_get_theme()->get('Version')
-    //     );
-    // }
-
-    // add_action( 'wp_enqueue_scripts', 'lighthouse_style_enqueue_styles' );
-
-    add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-    function enqueue_parent_styles() {
-        wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-    }    
+    add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
+    
+    function enqueue_child_theme_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    }
 ?>
